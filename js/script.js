f725bae0d788519c863ee4f2ba9e3419190c9b0a
yevheniia <evgesha.drozdova@gmail.com>
2018-12-20 16:40:27 +0200

    + pan zoom clipping

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ var selected =  "Реєстрація місця проживання/переб
 
 var svg = d3.select("svg"),
     width =  0.9 * ww,
-    height = 0.9 * hh;
+    height = 0.8 * hh;
 
 var margin = {top: 10, right: (0.1 * width), bottom: (0.1 * width), left: 40};
 
@@ -190,16 +190,55 @@ retrieve_plot_data(function(data) {
         points_g.attr("transform", d3.event.transform);
     }
 
+
+
+    var swoopy = d3.swoopyDrag()
+        .x(function(d){
+            return xScale(parseDate(d.sepalWidth))})
+        .y(function(d){
+            return yScale(+d.sepalLength)})
+        .draggable(true)
+        .annotations(annotations);
+
+    var swoopySel = svg.append('g')
+        .attr("fill", "none")
+        .call(swoopy);
+
+    swoopySel.selectAll("path")
+        .each(function(d) {
+            d3.select(this)
+                .attr("stroke", function(d) {
+                    return d.fill;
+                })
+
+        });
+
+    swoopySel.selectAll('text')
+        .each(function(d){
+            d3.select(this)
+                .text('')
+                .attr("class", "wrappedText")
+                .style("font-weight", 400)
+                .attr("stroke", "none")
+                .attr("fill", function(d) {
+                    return d.fill;
+                })
+                .attr("font-size", function(d) {
+                    return d.size;
+                })
+                .tspans(d3.wordwrap(d.text, d.wrap, d.betweenBig)); //wrap after 20 char
+
+        });
+
     //DRAW SCATTER PLOT
     function update(selected) {
 
-
-
+       swoopy.x(function(d){
+                return xScale(parseDate(d.sepalWidth))})
+            .y(function(d){
+                return yScale(d.sepalLength)});
 
         xAxis.tickFormat(d3.timeFormat("%b"));
-
-        //reset zoom
-        // points_g.attr("transform", 'translate(' + margin.left + ',' + 12  + ') scale(1)');
 
 
         //get new Data
@@ -215,7 +254,7 @@ retrieve_plot_data(function(data) {
 
 
         var width =  0.9 * ww,
-            height = 0.9 * hh;
+            height = 0.8 * hh;
 
         svg.select('.x-axis').call(xAxis);
         svg.select('.y-axis').call(yAxis);
@@ -238,14 +277,11 @@ retrieve_plot_data(function(data) {
         var bubble = points_g.selectAll(".bubble")
             .data(filteredDataNew);
 
-        bubble.on("click", function(d) {
-            // alert(d.registration)
-        });
 
 
         // EXIT
         bubble.exit()
-        // .transition(t)
+        .transition(t)
             .remove();
 
         // UPDATE
@@ -301,13 +337,11 @@ retrieve_plot_data(function(data) {
             });
 
 
-
         setTimeout(function(){
             svg.selectAll(".bubble").style("opacity", 1)
         }, 500);
 
-        points_g.selectAll(".bubble").on("click", function(d) {
-        });
+
 
         
         tippy('.bubble', {
@@ -323,119 +357,28 @@ retrieve_plot_data(function(data) {
             onShow(tip) {
                 tip.setContent(tip.reference.getAttribute('data-tippy-content'))
             }
-        
-        });
+         });
 
-
-
-
-
-
-
-
-
-        var swoopy = d3.swoopyDrag()
-            .x(function(d){
-
-                return xScale(parseDate(d.sepalWidth))})
-            .y(function(d){
-
-                return yScale(d.sepalLength)})
-            .draggable(true)
-            .annotations(annotations);
-
-        var swoopySel = svg.append('g')
-            .attr("fill", "none")
-            .call(swoopy);
-
-        swoopySel.selectAll("path")
-            .each(function(d) {
-                d3.select(this)
-                    .attr("stroke", function(d) {
-                        return d.fill;
-                    })
-
-            });
-
-        swoopySel.selectAll('text')
-            .each(function(d){
-                     d3.select(this)
-                        .text('')
-                        .attr("class", "wrappedText")
-                         .style("font-weight", 400)
-                        .attr("stroke", "none")
-                        .attr("fill", function(d) {
-                            return d.fill;
-                        })
-                        .attr("font-size", function(d) {
-                            return d.size;
-                        })
-                        .tspans(d3.wordwrap(d.text, d.wrap, d.betweenBig)); //wrap after 20 char
-
-            });
-
-
-
-
-
-
-
-    //     function zoomed() {
-    //
-    //         var cZ =  d3.event.transform.k;
-    //         console.log(cZ);
-    //
-    //         if(cZ < 2.5){
-    //             xAxis.tickFormat(d3.timeFormat("%b"))
-    //         } else if (cZ >=2.5 && cZ < 6 ){
-    //             xAxis.tickFormat(d3.timeFormat("%d.%m"))
-    //         } else {
-    //             xAxis.tickFormat(d3.timeFormat("%d.%m"))
-    //         }
-    //
-    //         var new_xScale = d3.event.transform.rescaleX(xScale);
-    //         var new_yScale = d3.event.transform.rescaleY(yScale);
-    //
-    //         gX.call(xAxis.scale(new_xScale));
-    //         gY.call(yAxis.scale(new_yScale));
-    //
-    //         var oneDay = 24 * 60 * 60 * 1000;
-    //         var daysAmount = Math.abs(new_xScale.domain()[0] - new_xScale.domain()[1]) / oneDay;
-    //
-    //         var bubbleZ = points_g.selectAll(".bubble");
-    //         bubbleZ.attr('width', (width / daysAmount) / 1.5)
-    //             .attr('height', (width / daysAmount) / 1.5)
-    //             .attr('x', function (d) {
-    //                 return new_xScale(d.registration);
-    //             })
-    //             .attr('y', function (d) {
-    //                 return new_yScale(d.counterTotal);
-    //             });
-    //     }
-    //
     }
 
 
-    $(window).on('resize', function() {
+    $(window).on('resize orientationchange', function() {
         var viewBox = $("#scatter")[0].getBoundingClientRect(),
             ww = viewBox.width,
             hh = viewBox.height;
 
         var width =  0.9 * ww,
-            height = 0.9 * hh;
+            height = 0.8 * hh;
 
         var xScale = fc.scaleDiscontinuous(d3.scaleTime())
             .discontinuityProvider(fc.discontinuitySkipWeekends());
 
-        xScale .domain([parseDate("2017-01-01"), parseDate("2017-12-31")])
+        xScale.domain([parseDate("2017-01-01"), parseDate("2017-12-31")])
             .range([0, width]);
 
         var yScale = d3.scaleLinear()
             .domain([0, 140])
             .range([height, 0]);
-
-
-
 
         svg.select('.x-axis').call(xAxis);
         svg.select('.y-axis').call(yAxis);
@@ -453,8 +396,15 @@ retrieve_plot_data(function(data) {
 
         legend.select('rect').attr('x', width);
 
-        legend.select('text').attr('x', width- 6)
-        ;
+        legend.select('text').attr('x', width- 6);
+
+
+        swoopySel.selectAll('text')
+            .each(function(d) {
+                d3.select(this)
+                    .text('')
+                    .tspans(d3.wordwrap(d.text, d.wrap, d.betweenBig)); //wrap after 20 char
+            })
 
     });
 
@@ -519,8 +469,8 @@ retrieve_plot_data(function(data) {
 
 var annotations = [
     {
-        "sepalWidth": "2017-01-5",
-        "sepalLength": 2,
+        "sepalWidth": "2017-01-05",
+        "sepalLength": 50,
         "path": "",
         "wrap": 20,
         "text": "Одна точка - одне звернення. Наведіть мишею на точки, аби побачити деталі. Графік масштабується колесиком миші",
@@ -530,9 +480,8 @@ var annotations = [
         "marker":"no",
         "betweenBig": 15,
         "textOffset": [
-            72,-426
+            63,-252
         ]
     }
-];
+]
 
-$(".bubble").off('click'); //disables click event
