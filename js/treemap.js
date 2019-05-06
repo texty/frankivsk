bbef8d639d5b499439ecf3ed10d022d78be9a9cb
YevhDr <evgesha.drozdova@gmail.com>
2019-05-07 00:05:35 +0300

    treemap text fit

--- js/treemap.js
+++ js/treemap.js
@@ -2,7 +2,12 @@
  * Created by yevheniia on 01.05.19.
  */
 
-var ratio = 1;
+var ratio;
+if(screen.width > 800) {
+    ratio = 2;
+} else {
+    ratio = 0.8
+}
 
 var red = "#3D728E";
 var green = "#CB93B2";
@@ -38,7 +43,7 @@ var treemap2 = d3.select("#my_dataviz2")
 
 
 // Read data
-d3.csv('data/top_frankivsk_amount.csv', function(treedata1) {
+d3.csv('data/top_frankivsk.csv', function(treedata1) {
 
     // stratify the data: reformatting for d3.js
     var root = d3.stratify()
@@ -55,12 +60,18 @@ d3.csv('data/top_frankivsk_amount.csv', function(treedata1) {
         .padding(4)
         (root);
 
-
-    // use this information to add rectangles:
-    treemap1
-        .selectAll("rect")
+    var nodes = treemap1.selectAll("g")
         .data(root.leaves())
         .enter()
+        .append("g");
+
+
+
+    // use this information to add rectangles:
+    nodes
+        // .selectAll("rect")
+        // .data(root.leaves())
+        // .enter()
         .append("rect")
         .attr("class", function(d) {
             return d.data.ind + " tree-nodes"
@@ -69,25 +80,16 @@ d3.csv('data/top_frankivsk_amount.csv', function(treedata1) {
         .attr("y", function(d) { return Math.round(d.y0) + "px"; })
         .attr("width", function(d) { return Math.round(d.x1 * ratio) - Math.round(d.x0 * ratio) + 1 + "px"; })
         .attr("height", function(d) { return Math.round(d.y1) - Math.round(d.y0) + "px"; })
+        // .attr("opacity", function(d) {
+        //     return d.data.opacity; })
         .attr("data-tippy-content", function(d) {  return "<b>Послуга:</b> " + d.data.id +
-            '<br> <b>Надано:</b> ' + d.data.size + " разів" +
-                '<br> <b>Вартість: </b>' + d.data.perItem
+            '<br> <b>Надано:</b> ' + d.data.size + " послуг" +
+                '<br> <b>Вартість: </b>' + d.data.perItem +
+                '<br> <b>Сума адміністративного збору: </b>' + d.data.sum + " грн"
 
         })
         .style("cursor", "pointer")
-        .style("fill", function(d) {  return treeColor(d.data.price)   })
-        .on("mouseover", function(d){
-            d3.select("#perItem").html("Вартість послуги: " + d.data.perItem);
-            var theClass= d3.select(this).attr("class").split(" ")[0];
-            console.log(theClass);
-            d3.selectAll("."+ theClass).style("opacity", 1)
-        })
-        .on("mouseout", function(d){
-            var theClass= d3.select(this).attr("class").split(" ")[0];
-            console.log(theClass);
-            d3.selectAll("."+ theClass).style("opacity", 0.8)
-        });
-    ;
+        .style("fill", function(d) {  return d.data.opacity   }) ;
 
     tippy('.tree-nodes', {
         theme:'tomato',
@@ -100,134 +102,53 @@ d3.csv('data/top_frankivsk_amount.csv', function(treedata1) {
     });
 
     // and to add the text labels
-    treemap1
-        .selectAll("treemap-text")
-        .data(root.leaves())
-        .enter()
-        .append("text")
+    if(window.innerWidth >= 800){
+        nodes
+            .append("text")
+            .style("font-size", "12px")
+            .attr("class", "treemap-text")
+            .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
+            .attr("y", function(d) { return Math.round(d.y0) + 15 + "px"; })
+            .text(function(d){
+                return d.data.short + "/ " + d.data.size  })
+            .call(wrap, 30)
+            .attr("opacity", getOpacity);
+    }
+    if(window.innerWidth < 800){
+        nodes
+            .append("text")
+            .style("font-size", "12px")
+            .attr("class", "treemap-text")
+            .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
+            .attr("y", function(d) { return Math.round(d.y0) + 15 + "px"; })
+            .text(function(d){
+                return d.data.short + "/ " + d.data.size  })
+            .call(wrap, 30)
+            .attr("opacity", getOpacity);
+     }
+});
 
-        .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
-        .attr("y", function(d) { return Math.round(d.y0) + 15 + "px"; })
-        .text(function(d){
-            return d.data.shortTitle + " " + d.data.size + " шт." })
-        .call(wrap, 100)
-        .attr("font-size", "10px")
-        .attr("fill", "rgb(72, 77, 96)")
-        .style("font-weight", "normal")
-})
+function getOpacity() {
+    var bbox = this.getBBox();
+    var cbbox = this.parentNode.querySelector('rect').getBBox();
+    var opacity;
 
+    if(bbox.height > cbbox.height || bbox.width > cbbox.width) {
+        opacity = 0;
+    } else {
+        opacity = 1;
+    }
+    return opacity;
 
-d3.csv('data/top_frankivsk_price.csv', function(treedata2) {
-
-    // stratify the data: reformatting for d3.js
-    var root = d3.stratify()
-        .id(function(d) { return d.id; })   // Name of the entity (column name is name in csv)
-        .parentId(function(d) { return d.parentId; })   // Name of the parent (column name is parent in csv)
-        (treedata2);
-
-    root.sum(function(d) { return +d.size })   // Compute the numeric value for each entity
-
-    // Then d3.treemap computes the position of each element of the hierarchy
-    // The coordinates are added to the root object above
-    d3.treemap()
-        .size([treemapWidth / ratio , treemapHeight])
-        .padding(4)
-        (root);
-
-
-    // use this information to add rectangles:
-    treemap2
-        .selectAll("rect")
-        .data(root.leaves())
-        .enter()
-        .append("rect")
-        .attr("class", function(d) {
-            return d.data.ind + " tree-nodes2"
-        })
-        .attr("x", function(d) { return Math.round(d.x0 * ratio) + "px"; })
-        .attr("y", function(d) { return Math.round(d.y0) + "px"; })
-        .attr("width", function(d) { return Math.round(d.x1 * ratio) - Math.round(d.x0 * ratio) + 1 + "px"; })
-        .attr("height", function(d) { return Math.round(d.y1) - Math.round(d.y0) + "px"; })
-        .attr("data-tippy-content", function(d) {  return d.data.id  })
-        .style("cursor", "pointer")
-        .style("fill", green )
-        .on("mouseover", function(d){
-            d3.select("#perItem").html("Вартість послуги: " + d.data.perItem)
-            var theClass= d3.select(this).attr("class").split(" ")[0];
-            console.log(theClass);
-            d3.selectAll("."+ theClass).style("opacity", 1)
-        })
-        .on("mouseout", function(d){
-            var theClass= d3.select(this).attr("class").split(" ")[0];
-            console.log(theClass);
-            d3.selectAll("."+ theClass).style("opacity", 0.8)
-        });
-
-    tippy('.tree-nodes2', {
-        delay: 50,
-        arrow: false,
-        size: 'big',
-        duration: 500,
-        distance: -50,
-        allowHTML: true
-    });
-
-    // and to add the text labels
-    treemap2
-        .selectAll("treemap-text")
-        .data(root.leaves())
-        .enter()
-        .append("text")
-        .attr("class", "treemap-text")
-        .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
-        .attr("y", function(d) { return Math.round(d.y0) + 5 + "px"; })
-        .html(function(d){ return d.data.size + " <br>" + "грн" })
-        .call(wrap, 20)
-        .attr("font-size", "13px")
-        .attr("fill", "rgb(72, 77, 96)")
-        .style("font-weight", "bold")
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
     function wrap(text, width) {
         text.each(function () {
             var text = d3.select(this),
-                words = text.text().split(/\s+/).reverse(),
-                word,
+                words = text.text().split(/\s+/).reverse();
+                var word,
                 line = [],
                 lineNumber = 0,
                 lineHeight = 1.1, // ems
@@ -242,7 +163,7 @@ d3.csv('data/top_frankivsk_price.csv', function(treedata2) {
             while (word = words.pop()) {
                 line.push(word);
                 tspan.text(line.join(" "));
-                if (tspan.node().getComputedTextLength() > width) {
+                if (tspan.node().getComputedTextLength() >= width) {
                     line.pop();
                     tspan.text(line.join(" "));
                     line = [word];
