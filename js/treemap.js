e54eb1169efd5f763af8154f107e5c99f7824421
YevhDr <evgesha.drozdova@gmail.com>
2019-05-07 12:07:57 +0300

    treemap legend

--- js/treemap.js
+++ js/treemap.js
@@ -5,29 +5,13 @@
 var ratio;
 if(screen.width > 800) {  ratio = 2;} else {  ratio = 0.8 }
 
-var treeViewBox = $("#my_dataviz1")[0].getBoundingClientRect();
-var treemapMargin = {top: 5, right: 10, bottom: 30, left: 10},
-    treemapWidth = treeViewBox.width - treemapMargin.left - treemapMargin.right,
-    treemapHeight = treeViewBox.height - treemapMargin.top - treemapMargin.bottom - 100,
-    treemapInnerHeight = treemapHeight * 0.90,
-    treeLegend;
-
 var treeColor = d3.scaleOrdinal()
     .domain(["~3 млн", "350 т.", "~300 т.", "~70 т.", "безкошт."])
     .range(["#8e677d", "#cb93b2", "#dbb3c9", "#dbb3c9", "#E6EFFF"]);
 
 
-var treemap1 = d3.select("#my_dataviz1")
-    .append("svg")
-    .attr("width", treemapWidth + treemapMargin.left + treemapMargin.right)
-    .attr("height", treemapHeight + treemapMargin.top + treemapMargin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + treemapMargin.left + "," + treemapMargin.top + ")");
-
-
 d3.csv('data/top_frankivsk.csv', function(treedata1) {
-    // stratify the data: reformatting for d3.js
+
     var root = d3.stratify()
         .id(function(d) { return d.id; })   // Name of the entity
         .parentId(function(d) { return d.parentId; })   // Name of the parent
@@ -35,103 +19,125 @@ d3.csv('data/top_frankivsk.csv', function(treedata1) {
 
     root.sum(function(d) { return +d.size });   // Compute the numeric value for each entity
 
-    // Then d3.treemap computes the position of each element of the hierarchy
-    // The coordinates are added to the root object above
-    d3.treemap()
-        .size([treemapWidth / ratio , treemapInnerHeight])
-        .padding(4)
-        (root);
+    function draw() {
+    // stratify the data: reformatting for d3.js
+        var treeViewBox = $("#my_dataviz1")[0].getBoundingClientRect();
+        var treemapMargin = {top: 5, right: 10, bottom: 30, left: 10},
+            treemapWidth = treeViewBox.width - treemapMargin.left - treemapMargin.right,
+            treemapHeight = treeViewBox.height - treemapMargin.top - treemapMargin.bottom - 100,
+            treemapInnerHeight = treemapHeight * 0.90,
+            treeLegend;
 
-    var nodes = treemap1.selectAll("g")
-        .data(root.leaves())
-        .enter()
-        .append("g");
+        var treemap1 = d3.select("#my_dataviz1")
+            .append("svg")
+            .attr("width", treemapWidth + treemapMargin.left + treemapMargin.right)
+            .attr("height", treemapHeight + treemapMargin.top + treemapMargin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + treemapMargin.left + "," + treemapMargin.top + ")");
 
-    nodes.append("rect")
-        .attr("class", function(d) { return d.data.ind + " tree-nodes"; })
-        .attr("x", function(d) { return Math.round(d.x0 * ratio) + "px"; })
-        .attr("y", function(d) { return Math.round(d.y0) + "px"; })
-        .attr("width", function(d) { return Math.round(d.x1 * ratio) - Math.round(d.x0 * ratio) + 1 + "px"; })
-        .attr("height", function(d) { return Math.round(d.y1) - Math.round(d.y0) + "px"; })
-        .attr("data-tippy-content", function(d) {  return "<b>Послуга:</b> " + d.data.id +
-            '<br> <b>Надано:</b> ' + d.data.size + " послуг" +
-                '<br> <b>Вартість: </b>' + d.data.perItem +
-                '<br> <b>Сума адміністративного збору: </b>' + d.data.sum + " грн"
-        })
-        .style("cursor", "pointer")
-        .style("fill", function(d) {  return d.data.opacity   }) ;
+        d3.treemap()
+            .size([treemapWidth / ratio , treemapInnerHeight])
+            .padding(4)
+            (root);
 
+        var nodes = treemap1.selectAll("g")
+            .data(root.leaves())
+            .enter()
+            .append("g");
 
-    /* окремі налаштування довжини тексту і легенди для різних екранів */
-    //середн+ та великі екрани
-    if(window.innerWidth >= 800){
-        nodes.append("text")
-            .style("font-size", "12px")
-            .attr("class", "treemap-text")
-            .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
-            .attr("y", function(d) { return Math.round(d.y0) + 15 + "px"; })
-            .text(function(d){
-                return d.data.short + "/ " + d.data.size  })
-            .call(wrap, 150)
-            .attr("opacity", getOpacity);
-
-
-        treeLegend = d3.legendColor()
-            .labelFormat(d3.format(".2f"))
-            .useClass(false)
-            .title("")
-            .titleWidth(200)
-            .orient('horizontal')
-            .shapeWidth(80)
-            .scale(treeColor);
-
-    }
-
-    //малі екрани
-    if(window.innerWidth < 800){
-        nodes.append("text")
-            .style("font-size", "11px")
-            .attr("class", "treemap-text")
-            .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
-            .attr("y", function(d) { return Math.round(d.y0) + 5 + "px"; })
-            .text(function(d){
-                return d.data.short + "/ " + d.data.size  })
-            .call(wrap, 30)
-            .attr("opacity", getOpacity);
+        nodes.append("rect")
+            .attr("class", function(d) { return d.data.ind + " tree-nodes"; })
+            .attr("x", function(d) { return Math.round(d.x0 * ratio) + "px"; })
+            .attr("y", function(d) { return Math.round(d.y0) + "px"; })
+            .attr("width", function(d) { return Math.round(d.x1 * ratio) - Math.round(d.x0 * ratio) + 1 + "px"; })
+            .attr("height", function(d) { return Math.round(d.y1) - Math.round(d.y0) + "px"; })
+            .attr("data-tippy-content", function(d) {  return "<b>Послуга:</b> " + d.data.id +
+                '<br> <b>Надано:</b> ' + d.data.size + " послуг" +
+                    '<br> <b>Вартість: </b>' + d.data.perItem +
+                    '<br> <b>Сума адміністративного збору: </b>' + d.data.sum + " грн"
+            })
+            .style("cursor", "pointer")
+            .style("fill", function(d) {  return d.data.opacity   }) ;
 
 
-        treeLegend = d3.legendColor()
-            .labelFormat(d3.format(".2f"))
-            .useClass(false)
-            .title("")
-            .titleWidth(200)
-            .orient('horizontal')
-            .shapeWidth((treemapWidth - 23) / 5)
-            .scale(treeColor);
-     }
+        /* окремі налаштування довжини тексту і легенди для різних екранів */
+        //середн+ та великі екрани
+        if(window.innerWidth >= 800){
+            nodes.append("text")
+                .style("font-size", "12px")
+                .attr("class", "treemap-text")
+                .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
+                .attr("y", function(d) { return Math.round(d.y0) + 15 + "px"; })
+                .text(function(d){
+                    return d.data.short + "/ " + d.data.size  })
+                .call(wrap, 150)
+                .attr("opacity", getOpacity);
 
 
-    //додаємо легенду
-    treemap1.append("g")
-        .attr("class", "treeLegend")
-        .attr("transform", "translate("+ 10 +',' + (treemapInnerHeight + 20) + ")");
+            treeLegend = d3.legendColor()
+                .labelFormat(d3.format(".2f"))
+                .useClass(false)
+                .title("")
+                .titleWidth(200)
+                .orient('horizontal')
+                .shapeWidth(80)
+                .scale(treeColor);
 
-    treemap1.select(".treeLegend")
-        .call(treeLegend);
+        }
+
+        //малі екрани
+        if(window.innerWidth < 800){
+            nodes.append("text")
+                .style("font-size", "11px")
+                .attr("class", "treemap-text")
+                .attr("x", function(d) { return Math.round(d.x0 * ratio) + 5 + "px"; })
+                .attr("y", function(d) { return Math.round(d.y0) + 5 + "px"; })
+                .text(function(d){
+                    return d.data.short + "/ " + d.data.size  })
+                .call(wrap, 30)
+                .attr("opacity", getOpacity);
 
 
-    //додаєо tooltips
-    tippy('.tree-nodes', {
-        theme:'tomato',
-        delay: 50,
-        arrow: false,
-        size: 'big',
-        duration: 500,
-        distance: -50,
-        allowHTML: true
+            treeLegend = d3.legendColor()
+                .labelFormat(d3.format(".2f"))
+                .useClass(false)
+                .title("")
+                .titleWidth(200)
+                .orient('horizontal')
+                .shapeWidth((treemapWidth - 23) / 5)
+                .scale(treeColor);
+         }
+
+        //додаємо легенду
+        treemap1.append("g")
+            .attr("class", "treeLegend")
+            .attr("transform", "translate("+ 10 +',' + (treemapInnerHeight + 20) + ")");
+
+        treemap1.select(".treeLegend")
+            .call(treeLegend);
+
+        //додаєо tooltips
+        tippy('.tree-nodes', {
+            theme:'tomato',
+            delay: 50,
+            arrow: false,
+            size: 'big',
+            duration: 500,
+            distance: -50,
+            allowHTML: true
     });
-
+}
+    draw();
+    
+    $(window).on('resize orientationchange', function() {
+        //перемальовуємо
+        $("#my_dataviz1").find("svg").remove()
+        draw();
+    });
 });
+
+
+
 
 
 
