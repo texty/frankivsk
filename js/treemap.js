631deeeefc56953c8e304bdc13c422bb502aeb5f
YevhDr <evgesha.drozdova@gmail.com>
2019-05-14 21:28:40 +0300

    logo

--- js/treemap.js
+++ js/treemap.js
@@ -7,7 +7,7 @@ if(screen.width > 800) {  ratio = 2;} else {  ratio = 0.8 }
 
 var treeColor = d3.scaleOrdinal()
     .domain(["~3 млн", "350 т.", "~300 т.", "~70 т.", "безкошт."])
-    .range(["#8e677d", "#cb93b2", "#d09eba", "#dbb3c9", "#E6EFFF"]);
+    .range(["#00868E", "#12A698", "#4FC595", "#8EE28A", "#E6EFFF"]);
 
 
 d3.csv('data/top_frankivsk.csv', function(treedata1) {
